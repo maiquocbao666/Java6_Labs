8540b47e5a151015e68ae2aa93a469b11ed1bfcd
maiquocbao666 <maiquocbao666@gmail.com>
2023-07-11 20:52:55 +0700

    Commit lab4

---
+++ baomqpc03196_lab4_sof306_sd17301/src/test/java/com/poly/baomqpc03196_lab4_sof306_sd17301/Baomqpc03196Lab4Sof306Sd17301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.baomqpc03196_lab4_sof306_sd17301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baomqpc03196Lab4Sof306Sd17301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
